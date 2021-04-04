9844cc23f1c2cf8dbc4595b2475eebd2259e506b
acetylme <i@acetyl.me>
2021-04-04 13:58:06 -0400

    Finish the pop-up modal to install templates. - Add the BootStrap modal component. - Write handleShow() and handleHide() behaviour. - Create new <TemplateInfo/> to show the detail of the installation.

--- src/components/TemplateCard.js
+++ src/components/TemplateCard.js
@@ -1,22 +1,55 @@
 import React from 'react'
-import {Card} from "react-bootstrap";
+import {Card, Button, Modal} from "react-bootstrap";
+import TemplateInfo from "./TemplateInfo";
 
 class TemplateCard extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            show: false
+        };
+    }
+
+    handleShow = () => {
+        this.setState({
+            show: true
+        })
+    };
+
+    handleHide = () => {
+        this.setState({
+            show: false
+        })
+    };
+
+
     render() {
         return (
+            <>
             <Card>
                 <Card.Img variant="top" src={this.props.data.info.icon} />
                 <Card.Body>
                     <Card.Title>{this.props.data.info.name}</Card.Title>
                     <Card.Text>
-                        {this.props.data.info.description}
+                        {this.props.data.info.description.slice(0,200)}
                     </Card.Text>
                 </Card.Body>
                 <Card.Footer>
-                    <small className="text-muted">Last updated 3 mins ago</small>
+                    <Button variant="success" onClick={this.handleShow}>Install</Button>
                 </Card.Footer>
             </Card>
+            <Modal show={this.state.show} onHide={this.handleHide}>
+                    <Modal.Header closeButton>
+                        <Modal.Title id="contained-modal-title-vcenter">
+                            {this.props.data.info.name}
+                        </Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <TemplateInfo data={this.props.data}/>
+                    </Modal.Body>
+                </Modal>
+            </>
         )
     }
 }
