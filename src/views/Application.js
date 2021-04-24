42fa087cb045083115578c750b82b34ff586c6f6
acetylme <i@acetyl.me>
2021-04-24 12:30:42 -0400

    Finish the application page to show all existing applications.

--- src/views/Application.js
+++ src/views/Application.js
@@ -1,11 +1,82 @@
 import React from "react";
-
+import {Col, Container, Row, Table,Badge,Button,ButtonGroup} from "react-bootstrap";
+import axios from "axios";
 class Application extends React.Component {
     render() {
         return (
-            <h1>Application Page</h1>
+            <Container>
+                <Row style={{'marginTop': 30}}>
+                    <Col>
+                        <Table striped bordered hover>
+                            <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Status</th>
+                                <th>Actions</th>
+                                <th>Edit</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            {this.renderTable()}
+                            </tbody>
+                        </Table>
+                    </Col>
+                </Row>
+            </Container>
+
+        )
+    }
+
+    renderTable() {
+        if(this.state.applications.length === 0){
+            return(
+                <tr>
+                    <td>No data</td>
+
+                </tr>
+            )
+        }else
+        {
+            return (
+                this.state.applications.map((application) =>
+                    <tr key={application.name}>
+                        <td><strong>{application.name}</strong></td>
+                        <td><Badge variant={application.status === 'running'?"success":"danger"}>{application.status}</Badge></td>
+                        <td>
+                            <Row>
+                                <Col>
+                                    <ButtonGroup><Button size="sm" variant="success">Start</Button>
+                                        <Button size="sm" variant="danger">Stop</Button></ButtonGroup>
+
+                                </Col>
+                            </Row>
+                        </td>
+                        <td>
+                            <Button size="sm" variant="info">Detail</Button>
+                            <Button size="sm" variant="danger">Delete</Button>
+                        </td>
+                    </tr>
+                )
+            )
+        }
+    }
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            applications: [
+            ],
+    }
+}
+    componentDidMount() {
+        axios.get(process.env.REACT_APP_API_SERVER.concat("/applications")).then(
+            (response) => {
+                this.setState({
+                    applications: response.data
+                })
+            }
         )
     }
 }
 
-export default Application
+export default Application;
